f71b3813b969302c6e2bd490e0f3a5f2e15931ae
majdeddine oumanni <majdeddineoumanni@gmail.com>
2025-05-12 01:47:56 +0100

    creating the main Entities Transaction, Budget and Category and linking them

--- back-end/src/main/java/com/budgetmanager/backend/Entities/Budget.java
+++ back-end/src/main/java/com/budgetmanager/backend/Entities/Budget.java
@@ -2,6 +2,8 @@ package com.budgetmanager.backend.Entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,4 +16,8 @@ import lombok.NoArgsConstructor;
 public class Budget {
     @Id
     private Long id;
+    private double limit;
+    @OneToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
